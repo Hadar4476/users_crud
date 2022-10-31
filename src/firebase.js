83050b4c328520e10911f861d4ff45dd99f115a7
Hadar4476 <hadar4476@gmail.com>
2022-10-31 09:40:07 +0200

    Added firebase to deploy app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyDU86eL0ygKAFeIM__xtaiqSQhnOC7ZYnY",
+  authDomain: "users-crud-9a8ab.firebaseapp.com",
+  projectId: "users-crud-9a8ab",
+  storageBucket: "users-crud-9a8ab.appspot.com",
+  messagingSenderId: "19302777682",
+  appId: "1:19302777682:web:12e5a2a934e0bf8139308b",
+};
+
+const firebaseApp = initializeApp(config);
